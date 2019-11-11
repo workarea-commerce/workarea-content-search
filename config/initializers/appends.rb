ca6539d423e331e6c461e4a5d41474dd4b43cb11
Tom Scott <tscott@weblinc.com>
2019-11-11 14:53:46 -0500

    Add Autocomplete to Content Search
    Using the shiny new **workarea-search-autocomplete** plugin, Content
    Search can now provide autocomplete results for the given search term.
    Content results are injected right below the common search terms, and
    augments the existing autocomplete with results from pages, blog posts,
    and other `Contentable` items.
    
    CONTSEARCH-3

--- config/initializers/appends.rb
+++ config/initializers/appends.rb
@@ -2,3 +2,15 @@ Workarea.append_partials(
   'storefront.above_search_results',
   'workarea/storefront/searches/search_type_toggle'
 )
+
+if Workarea::Plugin.installed?(:search_autocomplete)
+  Workarea.append_stylesheets(
+    'storefront.components',
+    'workarea/storefront/content_search/components/search_autocomplete'
+  )
+
+  Workarea.append_partials(
+    'storefront.search_autocomplete_under_searches',
+    'workarea/storefront/searches/autocomplete_content'
+  )
+end
